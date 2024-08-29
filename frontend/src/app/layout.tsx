1db5804462635ec3f78cedd2f0300b660a58a3b3
unstalling_user <dedtoud33@gmail.com>
2024-08-29 20:28:17 +0500

    add reset.css file and display transform

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "./reset.css";
 import React from "react";
 import Root from "../components/Root";
 
