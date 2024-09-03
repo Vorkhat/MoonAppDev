486ec373b3eec319a0de40b38ac2c6b45b94bab2
unstalling_user <dedtoud33@gmail.com>
2024-09-03 10:32:53 +0500

    add undefined photo user

--- frontend/src/app/api/userPhoto/[userId]/route.ts
+++ frontend/src/app/api/userPhoto/[userId]/route.ts
@@ -1,26 +1,36 @@
+import fs from 'fs';
+import path from 'path';
 import {NextRequest, NextResponse} from 'next/server';
 import {getUserAvatar} from "@/utils/UserPhoto/userPhoto.tsx";
 
 export async function GET(req: NextRequest, { params }: { params: { userId: number } }) {
-    if (!params.userId || isNaN(params.userId)) {
+    const userId = Number(params.userId);
+
+    if (!userId) {
         return NextResponse.json({ error: 'Invalid or missing userId' }, { status: 400 });
     }
 
     try {
-        const {url, key} = await getUserAvatar(params.userId);
+        const { url } = await getUserAvatar(userId);
         const response = await fetch(url);
-
         const imageBuffer = await response.arrayBuffer();
+
         return new NextResponse(imageBuffer, {
             headers: {
                 'Content-Type': response.headers.get('Content-Type') || 'image/jpeg',
                 'Cache-Control': 'private, max-age=3600',
-                'Content-Length': imageBuffer.byteLength.toString(),
             },
         });
 
-    } catch (error) {
-        console.error(error)
-        return NextResponse.json({ error: 'Failed to fetch user photo' }, { status: 500 });
+    } catch {
+        const failImagePath = path.join(process.cwd(), 'public/images', 'Avatar.png');
+        const failImageBuffer = fs.readFileSync(failImagePath);
+
+        return new NextResponse(failImageBuffer, {
+            headers: {
+                'Content-Type': 'image/jpeg',
+                'Cache-Control': 'private, max-age=3600',
+            },
+        });
     }
 }
