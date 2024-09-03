5f50730bde98b779447488705725b80f6ab4b256
zznty <94796179+zznty@users.noreply.github.com>
2024-09-03 17:40:33 +0700

    add prisma and initData signing

---
+++ frontend/prisma/views/public/UsersTop.sql
@@ -0,0 +1,6 @@
+-- todo make it materialized later for prod
+-- todo add cron job to refresh materialized view
+create view "UsersTop" as
+    select id, points,
+           row_number() over (order by points desc) as rank
+    from "User"
