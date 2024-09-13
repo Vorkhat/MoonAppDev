323277070c82b35c807abcb27a73151c9ba88ca2
zznty <94796179+zznty@users.noreply.github.com>
2024-09-13 14:28:16 +0700

    localize rank label in profile

--- frontend/src/components/pages/Profile/data/UserProfile.tsx
+++ frontend/src/components/pages/Profile/data/UserProfile.tsx
@@ -6,22 +6,25 @@ import ProfileAward from '@/components/pages/Profile/award/ProfileAward.tsx';
 import ProfileFriends from '@/components/pages/Profile/friends/ProfileFriends.tsx';
 import { Inter, Montserrat } from 'next/font/google';
 import { SessionData, useSession } from '@/components/session';
+import { getTranslations } from 'next-intl/server';
 
 const montserrat = Montserrat({ subsets: [ 'latin' ] });
 const inter = Inter({ subsets: [ 'latin' ] });
 
 export async function getUser(id: number) {
     return prisma.usersTop.findUniqueOrThrow({
-                                                 where: {
-                                                     id: id,
-                                                 },
-                                             });
+        where: {
+            id: id,
+        },
+    });
 }
 
 const UserProfile = async () => {
     const session: SessionData = await useSession();
 
     const user = await getUser(session.userId);
+
+    const t = await getTranslations('Profile');
 
     return (
         <div className={styles.user__profile}>
@@ -38,7 +41,9 @@ const UserProfile = async () => {
                         className={`${styles.user__name} ${inter.className}`}>{session.firstName} {session.lastName}</div>
                     {session.username ? <div
                         className={`${styles.user__tag} ${inter.className}`}>@{session.username}</div> : <></>}
-                    <div className={`${styles.user__rating} ${inter.className}`}>#{user.rank} в рейтинге</div>
+                    <div className={`${styles.user__rating} ${inter.className}`}>{
+                        t('content.Rank', { rank: user.rank })
+                    }</div>
                 </div>
             </div>
             <div className={styles.user__balance_border}>
