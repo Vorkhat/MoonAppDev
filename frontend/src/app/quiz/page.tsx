7020d67f2a52d54b95db5f5f68fb622388a73dd0
unstalling_user <dedtoud33@gmail.com>
2024-09-15 03:52:39 +0500

    add undefined quiz

--- frontend/src/app/quiz/page.tsx
+++ frontend/src/app/quiz/page.tsx
@@ -5,6 +5,8 @@ import CreateQuizComponent from '@/components/pages/Quiz/CreateQuizComponent';
 import { prisma } from '@/prisma';
 import { useSession } from '@/components/session';
 import { getTranslations } from 'next-intl/server';
+import { redirect } from 'next/navigation';
+import ContainerContent from '@/components/pages/common/components/ContainerContent/ContainerContent';
 
 const inter = Inter({ subsets: [ 'latin' ] });
 
@@ -31,6 +33,7 @@ export default async function Quiz() {
     });
 
     const t = await getTranslations('Quiz');
+    const s = await getTranslations('QuizUndefined');
 
     return (
         <div className={styles.quiz_page}>
@@ -46,11 +49,28 @@ export default async function Quiz() {
                         }}>
                             {t('header.title')}<br/>
                             <span className={styles.header__text}>{t('header.content')}</span><br/>
-                            <span className={`${styles.header__text} ${styles.text__color}`}> {t(
-                                'header.footer')}</span>
+                            <span className={`${styles.header__text} ${styles.text__color}`}> {t('header.footer')}</span>
                         </p>
                     </> :
-                    <></>
+                    <>
+                        <div className={`${styles.QuizUndefined} ${inter.className}`}>
+                            <span style={{
+                                color: 'var(--text-color)',
+                                fontSize: '1.5em',
+                                fontWeight: 'bold',
+                            }}>{s('header')}
+                            </span>
+                            <ContainerContent>
+                                <div className={styles.quizContent}>
+                                    <span className={styles.textContent}
+                                          style={{
+                                              fontSize: '1.3em',
+                                              fontWeight: 'bold'
+                                    }}>{s('content')}</span>
+                                </div>
+                            </ContainerContent>
+                        </div>
+                    </>
                 }
             </div>
             <div className={styles.quiz__list}>
