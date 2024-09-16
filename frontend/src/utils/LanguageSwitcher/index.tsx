238fbc27e2a552c8b30ffef5086e60e67fa1d23b
unstalling_user <dedtoud33@gmail.com>
2024-09-17 03:31:20 +0500

    fix cookies language switcher

--- frontend/src/utils/LanguageSwitcher/index.tsx
+++ frontend/src/utils/LanguageSwitcher/index.tsx
@@ -1,10 +1,10 @@
 import { prisma } from '@/prisma.ts';
-import { getCurrentLanguage, getCurrentSessionLanguage, setCurrentSessionLanguage } from '@/locale/locale';
+import { getCurrentSessionLanguage, setCurrentSessionLanguage } from '@/locale/locale';
 import { useSession } from '@/components/session';
 
 export default async function LanguageSwitcher() {
 
-    const language = getCurrentLanguage();
+    const language = await getCurrentSessionLanguage();
 
     async function saveLanguage() {
         'use server';
@@ -23,12 +23,12 @@ export default async function LanguageSwitcher() {
 
     return (
         <>
-            <form style={{
-                margin: '0 auto',
-            }} action={saveLanguage}>
+            <form style={{ margin: '0 auto' }} action={saveLanguage}>
                 <button type="submit" style={{
                     background: 'rgba(46, 42, 49, 0.24)',
                     border: '1px solid rgba(46, 42, 49, 0.24)',
+                    borderRadius: '1.5vw',
+                    padding: '0.5vh 1vw',
                     color: '#FFFFFF',
                 }} value={language}>
                     {language === 'Ru' ? 'EN' : 'RU'}
