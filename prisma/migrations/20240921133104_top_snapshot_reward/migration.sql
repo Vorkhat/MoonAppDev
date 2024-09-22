c9d0a473646009a27c3d34bd321483bd29e77ab4
unstalling_user <dedtoud33@gmail.com>
2024-09-22 22:30:02 +0500

    fix migration

--- prisma/migrations/20240921133104_top_snapshot_reward/migration.sql
+++ prisma/migrations/20240921133104_top_snapshot_reward/migration.sql
@@ -1,8 +1,2 @@
-/*
-  Warnings:
-
-  - Added the required column `reward` to the `TopSnapshot` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- AlterTable
-ALTER TABLE "TopSnapshot" ADD COLUMN     "reward" BIGINT NOT NULL;
+ALTER TABLE "TopSnapshot" ADD COLUMN     "reward" BIGINT NOT NULL DEFAULT 0;
