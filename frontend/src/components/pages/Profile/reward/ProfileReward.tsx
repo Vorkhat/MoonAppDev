789700ea69e6bf6094ff95c63664b69c84a6b61b
unstalling_user <dedtoud33@gmail.com>
2024-09-23 13:40:27 +0500

    fix count friends

--- frontend/src/components/pages/Profile/reward/ProfileReward.tsx
+++ frontend/src/components/pages/Profile/reward/ProfileReward.tsx
@@ -17,19 +17,32 @@ export async function getItems() {
                                               });
 }
 
+export async function getFriends() {
+    const session = await useSession();
+    return prisma.invitation.findMany({
+                                          where: {
+                                              userId: session.userId,
+                                          },
+                                          select: {
+                                              useCount: true,
+                                          },
+                                      });
+}
+
 export default async function ProfileReward() {
     const completedTaskTypes = await getItems();
+    const friendsCount = await getFriends();
 
     return (
         <div className={styles.rewardItems}>
             {
                 completedTaskTypes.map(value => (
                     <RewardItem image={mapToIcon(value.type)}
-                               key={value.type}
-                               type={value.type}
-                               count={value.count}
-                               number={value.reward ?? 0}
-                               currency={'points'}/>
+                                key={value.type}
+                                type={value.type}
+                                count={friendsCount[0]?.useCount ?? 0}
+                                number={value.reward ?? 0}
+                                currency={'points'}/>
                 ))
             }
         </div>
