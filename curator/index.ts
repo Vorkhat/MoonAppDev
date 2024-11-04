1255019ba5f6c6f7c72144427f546b93a90f1512
unstalling_user <dedtoud33@gmail.com>
2024-11-04 09:37:50 +0500

    fix curator

--- curator/index.ts
+++ curator/index.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 import { schedule } from 'node-cron';
 import updateCuratedTasks, { createCuratedTasks, createCuratedTasksForTask } from './tasks/updateCuratedTasks';
 import collectTopSnapshot from '@/tasks/collectTopSnapshot';
-import { consumeMessages } from '@/mq';
+import { consumeMessages } from 'frontend/src/mq.js';
 
 export const prisma = new PrismaClient();
 
